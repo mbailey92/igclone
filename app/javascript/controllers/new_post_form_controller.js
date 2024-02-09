00609f9c225284f31ae943a23309dbdc7548a044
Michael Bailey <michaelbailey92@gmail.com>
2024-02-09 16:11:25 +0000

    created form to select photo from computer and submit, form hidden, using new post form stimulus controller to target hidden form actions

---
+++ app/javascript/controllers/new_post_form_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="new-post-form"
+export default class extends Controller {
+static targets = ['selectFileButton', 'photoFile'];
+
+  connect() {
+  }
+
+  selectFile(e) {
+    e.preventDefault();
+    this.photoFileTarget.click();
+  }
+
+  submitForm() {
+    this.element.sunbmit();
+  }
+}
